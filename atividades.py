538ac22905607d48a0a12a0146ff48ad487f42d3
Gb <joaogabrielmcpe110@gmail.com>
2022-10-26 22:23:13 -0300

    testando

--- atividades.py
+++ atividades.py
@@ -1,1 +1,18 @@
-print("alterando coisa")
+lista = [10,11,20,22,30,33,40,44,50,55,60,66,70,77,80,88,90,99]
+
+for x in lista:
+    if x % 2 == 0:
+        print("numeros pares {}" .format(x))
+for x in lista:
+    if x % 2 == 1:
+        print("numeros impares {}" .format(x))
+for x in lista:
+    if x % 2 == 0:
+        print("numeros multiplos de 2 {}" .format(x))
+
+#desafio
+
+#peça quatro valores. calcule e imprima o volume da caixa retangualar informada
+#ultilizando a formula
+#volume = comprimento x largura x altura
+
