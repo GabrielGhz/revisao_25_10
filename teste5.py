e9c899832145e1fe2df1cdf110821fde8506bbb3
Gb <joaogabrielmcpe110@gmail.com>
2022-10-29 19:09:56 -0300

    adicionado novo teste para revisão

---
+++ teste5.py
@@ -0,0 +1,11 @@
+#Faça uma função que receba um valor para imprimir no formato abaixo:
+#1
+#2 2
+#3 3 3
+#4 4 4 4
+#5 5 5 5 5 5
+
+numero = input("Digite um número!! \n:")
+str_numero = numero + " "
+int_numero = int(numero)
+print(int_numero * str_numero)
